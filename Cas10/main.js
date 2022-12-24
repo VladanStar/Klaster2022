aefb09203b5d22956197e8cf04ba01ca9c07290d
VladanStar <vladancupric@gmail.com>
2022-12-24 11:01:14 +0100

    Add

--- Cas10/main.js
+++ Cas10/main.js
@@ -1,18 +1,32 @@
 console.log(4 + "3");
+console.log("4" + "3");
 
-/*---------*/ 
-var a =2;
-function arr (){
-   var a =4
+/*---------*/
+var a = 2;
+function arr() {
+  var a = 4;
 }
 arr();
-console.log(a)
+console.log(a);
 
-console.log(10%3)
+console.log(10 % 3);
 
-console.log("string" + "java")
-console.log(true + true)
+console.log("string" + "java");
+console.log(true + true);
 console.log(true * true);
-console.log(true ===1)
-console.log(true  + 1)
-console.log(0 ===33)
+console.log(true === 1);
+console.log(true + 1);
+console.log(0 === 33);
+let ime = "vladan";
+let prezime = " cupric";
+let fullName = ime + prezime;
+console.log(prezime);
+console.log(fullName);
+let b = 34;
+b = 22;
+console.log(a + b);
+ const c = 23;
+//  c =22;
+ console.log(b +c);
+//  const c = 24;
+ b = 9

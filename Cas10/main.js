babb1cbf62925818ed365eed97a9a48eb6e3161c
VladanStar <vladancupric@gmail.com>
2022-12-24 10:39:28 +0100

    Add

---
+++ Cas10/main.js
@@ -0,0 +1,15 @@
+console.log(4 + "3");
+
+/*---------*/ 
+var a =2;
+function arr (){
+   var a =4
+}
+arr();
+console.log(a)
+
+console.log(10%3)
+
+console.log("string" + "java")
+console.log(true + true)
+console.log(true * true)

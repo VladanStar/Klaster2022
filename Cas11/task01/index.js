b14b2332664a0d36553ae27baacda0080940fadd
VladanStar <vladancupric@gmail.com>
2022-12-25 10:38:12 +0100

    Add

--- Cas11/task01/index.js
+++
@@ -1,5 +0,0 @@
-let ime = "Pera";
-let age = 34;
-console.log('name: ',ime);
-console.log( ime, typeof ime);
-console.log(age ,typeof age);
---
+++ Cas11/task01/main.js
@@ -0,0 +1,16 @@
+let ime = "Pera";
+let age = 34;
+console.log("name: ", ime);
+console.log(ime, typeof ime);
+console.log(age, typeof age);
+
+// let c = prompt("unesi prvi broj: ");
+// let d = prompt("unsi drugi  broj: ");
+
+// let zbir = Number(c) + Number(d);
+// alert("zbir je " + zbir);
+
+let godina = 2022;
+let godRodjenja = prompt("Unesite godinu rodjenja:");
+let starost = godina - godRodjenja;
+alert("Vi i mate  " + starost);

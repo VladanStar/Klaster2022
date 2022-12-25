c9a2c36ecb5bb9c759e40cb166c43e940ae951d4
VladanStar <vladancupric@gmail.com>
2022-12-25 10:20:03 +0100

    Add

---
+++ Cas11/task01/index.js
@@ -0,0 +1,16 @@
+
+"use strict";
+
+let a = 23;
+let b = 4;
+// let c = a + b;
+// console.log("ovo su promenljive ", a, b);
+// console.log( "ovo je zbir", c)
+// console.log(false && false);
+// prompt ("vv");
+
+let c = prompt("unesi prvi broj: ");
+let d = prompt("unsi drugi  broj: ");
+
+let zbir = Number(c) + Number(d);
+alert("zbir je " + zbir);

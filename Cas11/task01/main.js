874f07f51f413c4b290172f10b0ed0dd3c2d3994
VladanStar <vladancupric@gmail.com>
2022-12-25 11:46:49 +0100

    Add

--- Cas11/task01/main.js
+++ Cas11/task01/main.js
@@ -1,8 +1,8 @@
-let ime = "Pera";
-let age = 34;
-console.log("name: ", ime);
-console.log(ime, typeof ime);
-console.log(age, typeof age);
+// let ime = "Pera";
+// let age = 34;
+// console.log("name: ", ime);
+// console.log(ime, typeof ime);
+// console.log(age, typeof age);
 
 // let c = prompt("unesi prvi broj: ");
 // let d = prompt("unsi drugi  broj: ");
@@ -10,7 +10,21 @@ console.log(age, typeof age);
 // let zbir = Number(c) + Number(d);
 // alert("zbir je " + zbir);
 
-let godina = 2022;
-let godRodjenja = Number(prompt("Unesite godinu rodjenja:"));
-let starost = godina - godRodjenja;
-alert(`"Vi i mate  " ${starost} " godina";`)
+// let godina = 2022;
+// let godRodjenja = Number(prompt("Unesite godinu rodjenja:"));
+// let starost = godina - godRodjenja;
+// alert(`"Vi i mate  " ${starost} " godina";`);
+// document.getElementById("btn").onclick = calculateBMI;
+
+function calculateBMI() {
+  let kg = document.getElementById("kilo").value;
+  let visina = document.getElementById("visina").value;
+  let bmi = kg / (visina * 2);
+  let result = document.getElementById("result");
+  result.innerHTML += `${bmi}`;
+  console.log(bm);
+}
+// console.log(bmi(kg, visina))
+
+// console.log(bmi(kg, visina))
+//  let godine = prompt("Unesi svoje godine:");

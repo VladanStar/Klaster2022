36add457aa392c8c4cac078d6d28dc7b23c9a179
VladanStar <vladancupric@gmail.com>
2023-01-09 16:02:44 +0100

    Add

--- HomeWork20230109/task02/main.js
+++ HomeWork20230109/task02/main.js
@@ -1,23 +1,17 @@
 function deljiv() {
-  let broj = document.getElementById("broj").value;
+  let ocena = document.getElementById("ocena").value;
   let result = document.getElementById("result");
-  switch (broj) {
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-      result.innerHTML += ` Za dobijenu ocenu: ${broj} ste Pali ispit`;
-      break;
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-    case 10:
-      result.innerHTML += ` Za dobijenu ocenu: ${broj} ste Prosliispit`;
-      break;
-    default:
-      alert("Unesite vrednost vecu od 1 i manju od 10")
-      break;
+  if(ocena >1 && ocena <= 5 ){
+      result.innerHTML += ` Za dobijenu ocenu: ${ocena} ste Pali ispit`;
   }
-}
+  else if(ocena >=6 && ocena <=10){
+  
+      result.innerHTML += ` Za dobijenu ocenu: ${ocena} ste Prosli ispit`;
+  
+  }
+  else{
+    alert("Unesite vrednost vecu od 1 i manju od 10")
+  }
+   
+  }
+

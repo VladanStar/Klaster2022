14ed6123a5ebc69a3c4f6825e43e18984d595b79
VladanStar <vladancupric@gmail.com>
2023-01-09 15:56:57 +0100

    Add

--- HomeWork20230109/task02/main.js
+++ HomeWork20230109/task02/main.js
@@ -1,14 +1,23 @@
-
-
-function deljiv(){
-    let broj = document.getElementById('broj').value;
-    let result = document.getElementById('result');
-    if (broj % 3 === 0) {
-    //  alert("Divisible by 3");
-  
-  result.innerHTML += ` Uneti broj: ${broj} je deljiv sa 3`;
-    } else {
-    //  alert("Not divisible by 3");
-    result.innerHTML += ` Uneti broj: ${broj} nije deljiv sa 3`;
-    }
+function deljiv() {
+  let broj = document.getElementById("broj").value;
+  let result = document.getElementById("result");
+  switch (broj) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      result.innerHTML += ` Za dobijenu ocenu: ${broj} ste Pali ispit`;
+      break;
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+      result.innerHTML += ` Za dobijenu ocenu: ${broj} ste Prosliispit`;
+      break;
+    default:
+      alert("Unesite vrednost vecu od 1 i manju od 10")
+      break;
   }
+}

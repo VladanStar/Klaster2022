447cd1c0ee266038509e8609c4009ea9681dc41c
VladanStar <vladancupric@gmail.com>
2023-01-09 16:15:14 +0100

    Add

--- HomeWork20230109/task03/main.js
+++ HomeWork20230109/task03/main.js
@@ -1,9 +1,9 @@
 function paranIliNeparan() {
-  let ocena = document.getElementById("broj").value;
+  let broj = document.getElementById("broj").value;
   let result = document.getElementById("result");
   if (broj % 2 === 0) {
     result.innerHTML += ` Broj : ${broj} je paran`;
   } else {
-    result.innerHTML += ` Broj: ${ocena} je neparan`;
+    result.innerHTML += ` Broj: ${broj} je neparan`;
   }
 }

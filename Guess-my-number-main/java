e15d858c61e607b3ccf020fef1baf33641bd75a6
VladanStar <vladancupric@gmail.com>
2023-01-14 11:02:59 +0100

    Add

---
+++ Guess-my-number-main/java
@@ -0,0 +1,22 @@
+class prasad
+{
+    public static void main (String[] args)
+    {
+        // Area of circle
+        int r,area;
+        r=5;
+        area= 3.14 *r*r;
+        System.out.println("Area of Circle: " +area);
+
+        // Area of squre
+        
+         int a,SqArea;
+        a=5;
+        sqArea= a*a;
+        System.out.println("Area of Squre: " +sqArea);
+
+
+
+
+    }
+}
--- zadatak/script.js
+++ zadatak/script.js
@@ -16,6 +16,7 @@ document.querySelector(".check").addEventListener("click", function(){
         document.querySelector(".label-highscore").innerHTML =
         "Tacan  Number....Svaka cast pogodili ste";
         document.querySelector("body").style.backgroundColor = "#60b347";
+        document.querySelector(".number").innerHTML= `${probniBroj}`;
     }
     else if (probniBroj >zadatiNumber){
         document.querySelector(".label-highscore").innerHTML= "TOO High"

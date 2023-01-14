42e8e611769b4ce6aaad3e9db64995b057654ce8
VladanStar <vladancupric@gmail.com>
2023-01-14 10:54:37 +0100

    Add

---
+++ zadatak/script.js
@@ -0,0 +1,29 @@
+'use strict';
+let zadatiNumber = Math.round(Math.random() * 20 + 1);
+
+document.querySelector(".again").addEventListener("click", function () {
+    zadatiNumber = Math.round(Math.random() * 20 + 1);
+
+    document.querySelector("body").style.backgroundColor = "#222";
+
+})
+
+document.querySelector(".check").addEventListener("click", function(){
+    const probniBroj = Number(document.querySelector(".guess").value);
+
+    if(probniBroj  == zadatiNumber){
+        
+        document.querySelector(".label-highscore").innerHTML =
+        "Tacan  Number....Svaka cast pogodili ste";
+        document.querySelector("body").style.backgroundColor = "#60b347";
+    }
+    else if (probniBroj >zadatiNumber){
+        document.querySelector(".label-highscore").innerHTML= "TOO High"
+    }
+    else if (probniBroj < zadatiNumber){
+        document.querySelector(".label-highscore").innerHTML = "TOO Low"
+
+    }
+
+
+})

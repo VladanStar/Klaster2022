a72e96bda937c610eae7ecf8f06619851f145f02
VladanStar <vladancupric@gmail.com>
2023-01-14 11:27:10 +0100

    Add

--- zadatak/script.js
+++ zadatak/script.js
@@ -1,30 +1,53 @@
 'use strict';
 let zadatiNumber = Math.round(Math.random() * 20 + 1);
 
-document.querySelector(".again").addEventListener("click", function () {
-    zadatiNumber = Math.round(Math.random() * 20 + 1);
+let Scorevalue = 20;
 
-    document.querySelector("body").style.backgroundColor = "#222";
+let hiscore=0;
 
-})
+document.querySelector('.again').addEventListener('click', function () {
+  location.reload();
+});
 
-document.querySelector(".check").addEventListener("click", function(){
-    const probniBroj = Number(document.querySelector(".guess").value);
+document.querySelector('.check').addEventListener('click', function () {
+  const probniBroj = Number(document.querySelector('.guess').value);
+if(probniBroj >20 || probniBroj <1){
+    document.querySelector('.message').innerHTML = 'uneli ste Nekorektan Broj ';
+  
+}
+ else if (probniBroj == zadatiNumber) {
+    document.querySelector('.label-highscore').innerHTML =
+      'Tacan  Number....Svaka cast pogodili ste';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').innerHTML = `${probniBroj}`;
+    document.querySelector('.message').innerHTML = 'Pogodilli ste';
+    document.querySelector('h1').innerHTML = 'Pogodilli ste';
+  } else if (probniBroj > zadatiNumber) {
+    document.querySelector('.label-highscore').innerHTML = 'TOO High';
+    {
+        Scorevalue--;
+  
+        document.querySelector(".score").textContent = Scorevalue;
+        if (Scorevalue <= 0)
+        {
+          document.querySelector(".score").textContent = 0;
+          console.log
+          (document.querySelector(".label-highscore").textContent = "You lost the game");
+        }
+      }
+  } else if (probniBroj < zadatiNumber) {
+    document.querySelector('.label-highscore').innerHTML = 'TOO Low';
+  }
+  {
+    Scorevalue--;
 
-    if(probniBroj  == zadatiNumber){
-        
-        document.querySelector(".label-highscore").innerHTML =
-        "Tacan  Number....Svaka cast pogodili ste";
-        document.querySelector("body").style.backgroundColor = "#60b347";
-        document.querySelector(".number").innerHTML= `${probniBroj}`;
+    document.querySelector(".score").textContent = Scorevalue;
+    if (Scorevalue <= 0)
+    {
+      document.querySelector(".score").textContent = 0;
+      console.log
+      (document.querySelector(".label-highscore").textContent = "You lost the game klikni Ponovo button");
     }
-    else if (probniBroj >zadatiNumber){
-        document.querySelector(".label-highscore").innerHTML= "TOO High"
-    }
-    else if (probniBroj < zadatiNumber){
-        document.querySelector(".label-highscore").innerHTML = "TOO Low"
+  }
 
-    }
-
-
-})
+});
